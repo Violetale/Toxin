66ec4d02fb974bdc89827896757f0b951f962672
Amferiel <incanu94@mail.com>
2020-02-14 14:25:20 +0300

    Несколько попревлены стили в компоненте пагинация.

--- src/modules/range-slider/range-slider.js
+++ src/modules/range-slider/range-slider.js
@@ -1,8 +1,18 @@
 import './range-slider.scss';
 import $ from 'jquery';
   $(document).ready(function() {
-    $('.range-slider').ionRangeSlider({
-      min: 10,                        // минимальное значение
-      max: 100,                       // максимальное значение
-      from: 30});
+    $('.range-slider__input').ionRangeSlider({
+      type: 'double',
+      min: 0,                        // минимальное значение
+      max: 15000,                       // максимальное значение
+      from: 5000,
+      to: 10000,
+      onChange: function(data) {
+        /*$(this).find('.range-slider__value').html(data);
+        writeValue(data)*/
+    },
+    onStart: function(data) {
+        writeValue(data)
+    }
+    });
   });
