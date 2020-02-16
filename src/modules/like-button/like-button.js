b8c37b261265f9d9c90a565fb6b69f278dab0525
Amferiel <incanu94@mail.com>
2020-02-16 13:23:47 +0300

    Сделаны like-button

--- src/modules/like-button/like-button.js
+++ src/modules/like-button/like-button.js
@@ -1,1 +1,18 @@
 import './like-button.scss';
+import $ from 'jquery';
+$(document).ready(function(){
+    $('.like-button').on('click',like);
+    function like(e){
+        let self = $(this);
+        let likeCount = self.find('.like-button__count');
+        let likeCountVal =parseInt(likeCount.attr('data-value'));
+        if(self.hasClass('like-button_clicked')){
+            likeCountVal--;
+        }else{
+            likeCountVal++;
+        }
+        self.toggleClass('like-button_clicked');
+        likeCount.attr('data-value',likeCountVal);
+        likeCount.text(likeCountVal);
+    }
+});
