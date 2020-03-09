806639e131c28894e42909bc0c1ada9944c56235
Amferiel <incanu94@mail.com>
2020-03-09 13:56:18 +0300

    Верстка чекбоксов. Почти завершена

--- src/modules/field/field.js
+++ src/modules/field/field.js
@@ -2,4 +2,4 @@ import './field.scss';
 import 'jquery.maskedinput/src/jquery.maskedinput';
 
 const maskedFields = $('.text-field_masked');
-maskedFields.mask('99.99.9999');
+maskedFields.mask('31.12.9999');
