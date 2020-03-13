6b1828ffedf53c1e976dc5cdb9dfdf9b6eb4cc3b
Amferiel <incanu94@mail.com>
2020-03-13 14:01:08 +0300

    Сверстан checkbox-list,необходимо доделать ему  поведение

--- src/modules/checkbox-list/checkbox-list.js
+++ src/modules/checkbox-list/checkbox-list.js
@@ -1,1 +1,9 @@
 import './checkbox-list.scss';
+import $ from 'jquery';
+$(function(){
+    $('.checkbox-list_label').on('click',toggleCheckboxList);
+    function toggleCheckboxList(e){
+        let self = this;
+        self.toggleClass('open');
+    }
+});
