e1e9cd76193bcf53bf9a77cc4746be2ed2d0460a
Amferiel <incanu94@mail.com>
2020-03-14 13:56:33 +0300

    Добавлено поведение компоненту сруслищч-дшые

--- src/modules/checkbox-list/checkbox-list.js
+++ src/modules/checkbox-list/checkbox-list.js
@@ -1,9 +1,15 @@
 import './checkbox-list.scss';
 import $ from 'jquery';
 $(function(){
-    $('.checkbox-list_label').on('click',toggleCheckboxList);
+    $('.checkbox-list').on('click',toggleCheckboxList);
     function toggleCheckboxList(e){
         let self = this;
-        self.toggleClass('open');
+        $(self).toggleClass('checkbox-list_open');
+        let icon = $(self).find('i');
+        if($(icon).text()=='expand_less'){
+            $(icon.text('expand_more'));
+        }else {
+            $(icon).text('expand_less');
+        }
     }
 });
