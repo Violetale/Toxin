dadab096af82ca1e33829a8d26f7a48e902b066f
Amferiel <incanu94@mail.com>
2020-03-23 12:58:55 +0300

    Рефакториг dropdown

--- src/modules/dropdown/dropdown.js
+++ src/modules/dropdown/dropdown.js
@@ -1,8 +1,23 @@
 import './dropdown.scss';
 import $ from 'jquery';
-$(document).ready(function(){
-        $('.iqdropdown').iqDropdown();
-        let dropdownElem = $('.dropdown');
-        
-        
-});
+$(".custom").iqDropdown({
+        minItems: 1,
+        maxItems: 5,
+        selectionText: "passenger",
+        textPlural: "passengers",
+        onChange: function(id, count, totalItems) {
+          console.log(id, count, totalItems);
+        },
+        beforeDecrement: function(id, itemCount) {
+          if (id === "adult") {
+            return itemCount.adult > itemCount.infant;
+          }
+          return true;
+        },
+        beforeIncrement: function(id, itemCount) {
+          if (id === "infant") {
+            return itemCount.adult > itemCount.infant;
+          }
+          return true;
+        }
+      });
