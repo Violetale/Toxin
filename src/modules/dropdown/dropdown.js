0218fb4099c143510a416cd92db6b7ae2fae06f2
Amferiel <incanu94@mail.com>
2020-03-26 13:06:57 +0300

    рефакторинг и вёрстка dropdown 50%

--- src/modules/dropdown/dropdown.js
+++ src/modules/dropdown/dropdown.js
@@ -1,23 +1,32 @@
 import './dropdown.scss';
 import $ from 'jquery';
-$(".custom").iqDropdown({
-        minItems: 1,
-        maxItems: 5,
-        selectionText: "passenger",
-        textPlural: "passengers",
-        onChange: function(id, count, totalItems) {
-          console.log(id, count, totalItems);
-        },
-        beforeDecrement: function(id, itemCount) {
-          if (id === "adult") {
-            return itemCount.adult > itemCount.infant;
-          }
-          return true;
-        },
-        beforeIncrement: function(id, itemCount) {
-          if (id === "infant") {
-            return itemCount.adult > itemCount.infant;
-          }
-          return true;
-        }
-      });
+
+//функция, устанавливающая в data-атрибуты каждого dropdown-item
+// значения, необходимые для вывода в dropdown-selection
+//посредством showResult
+const setValue = function(dropdown,itemNem,itemCount){
+
+}
+const showItem=function(){}
+const showResult = function(dropdown){
+    let items = dropdown.find('.dropdown__item')
+}
+$('.dropdown').each(function(){
+  let dropdown = $(this);
+  let controls = $(dropdown).find('.controls');
+  $(".iqdropdown").iqDropdown({
+    onChange: function(itemName, itemCount, totalItems) {
+      setValue(dropdown,itemName,itemCount);
+      showItem(dropdown);
+      //
+      if(totalItems){
+        $(controls).find('.button-reset').removeClass('hidden');
+      }else{
+        $(controls).find('.button-reset').addClass('hidden');
+      }
+  }
+});
+});
+
+
+    //скрипты для поведения кнопок поведения
