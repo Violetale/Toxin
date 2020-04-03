960560f654e354b87ad5bbc4082e5c43c57be875
Amferiel <incanu94@mail.com>
2020-04-03 13:47:35 +0300

    Переписывается dropdown.js

--- src/modules/dropdown/dropdown.js
+++ src/modules/dropdown/dropdown.js
@@ -1,38 +1,57 @@
 import './dropdown.scss';
 import $ from 'jquery';
 
-//функция, устанавливающая в data-атрибуты каждого dropdown-item
-// значения, необходимые для вывода в dropdown-selection
-//посредством showResult
 $(function(){
-  const nouns={};//-написать
-  let resultString="";
-  const showResult = function(dropdown){
-    let selection = $(dropdown).find('.dropdown-selection');
-    $(selection).text("");
-    let items = $(dropdown).find('.iqdropdown-menu-option');
-    $(items).each(function(idx,elem){
-      let count = $(elem).find('.counter').text();
-      if(count!==0){
-      resultString +=count;
-      }
-    })
-    $(selection).text(resultString);
-}
-
-$('.dropdown').each(function(){
-  let dropdown = $(this);
-  let controls = $(dropdown).find('.controls');
-  $(".iqdropdown").iqDropdown({
-    onChange: function(itemName, itemCount, totalItems) {
-      showResult(dropdown);
-      if(totalItems){
-        $(controls).find('.button-reset').removeClass('hidden');
-      }else{
-        $(controls).find('.button-reset').addClass('hidden');
-      }
+ 
+  function correctNaming(itemName,itemCount){
+    let nounsString=$(`[data-id=${itemName}]`).attr('data-nouns');
+    let nouns = nounsString.split(',');
+    console.log(nouns);
+    if(itemCount==0){
+      return '';
+    }else if(itemCount==1){
+      return `${itemCount} ${nouns[0]}`
+    }else if(itemCount>1&&itemCount<=5){
+      return `${itemCount} ${nouns[1]}`
+    }else{
+      return `${itemCount} ${nouns[2]}`
+    }
   }
+
+  function setSelectionText(itemName,itemCount,totalItems,defaultText){
+    let resultString ="";
+    if(totalItems==0){
+      resultString = defaultText;
+    }else{
+      resultString = correctNaming(itemName,itemCount);
+    }
+    return `${resultString}`;
+  }
+  function showSelection(selection,string){
+      $(selection).html(string);
+  }
+  
+$('.dropdown').each(function(){
+  let $dropdown = $(this);
+  let $controls = $($dropdown).find('.controls');
+  let $selection =$($dropdown).find('.iqdropdown-selection');
+  let $defaultText= $($dropdown).find('.iqdropdown').attr('data-defaultText');
+  
+  $(".iqdropdown").iqDropdown({
+    onChange(itemName, itemCount, totalItems){ 
+      if(totalItems){
+        $($controls).find('.button-reset').removeClass('hidden');
+        
+      }else{
+        $($controls).find('.button-reset').addClass('hidden');
+      }
+      console.log(itemCount);
+      let $text = setSelectionText(itemName,itemCount,totalItems,$defaultText);
+      $($selection).html($text);
+  }
+  
 });
+showSelection($selection,$defaultText);
 });
 //-событие apply 
 //-событие принять
