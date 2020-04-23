850484f9a920b4025e6495307b6ec7250f7fc2e6
Amferiel <incanu94@mail.com>
2020-04-23 23:15:33 +0300

    Доработка dropdown-date. Прикручен календарь к двум полям. Разобраться почему обновляются ВСЕ поля на странице при выборе диапазона дат

--- src/modules/dropdown-date/dropdown-date.js
+++ src/modules/dropdown-date/dropdown-date.js
@@ -1,3 +1,28 @@
 import './dropdown-date.scss';
 import $ from 'jquery';
+$(()=>{
+  class DateDropdown {
+    constructor(component) {
+      this.component = component;
+      this.$start = $('.js-date-dropdown__start', this.$component);
+      this.$end = $('.js-date-dropdown__end', this.$component);
+      this._attachEventHandler(this.$start,this.$end);
+    }
+    _attachEventHandler(start,end){
+        $(start).datepicker({
+          todayButton: new Date(),
+          //-range: $(this).component,
+          onSelect: function (formattedDate, date, inst) {
+            const [ firstDate, secondDate ] = fd.split(inst.views.days.opts.multipleDatesSeparator);
+            $(start).val(firstDate);
+            $(end).val(secondDate);
+          },
+    }).data('datepicker');
+    }
+  }
+    $('.js-date-dropdown').each((index, node) => {
+      new DateDropdown(node);
+  });
+});
+
 
