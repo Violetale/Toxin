f98e4952f9a30eddfb6b3b79d2dff9261720b3d9
Amferiel <incanu94@mail.com>
2020-04-24 13:37:52 +0300

    Небольшие правки в dropdown-date

--- src/modules/dropdown-date/dropdown-date.js
+++ src/modules/dropdown-date/dropdown-date.js
@@ -1,27 +1,28 @@
 import './dropdown-date.scss';
 import $ from 'jquery';
+
 $(()=>{
-  class DateDropdown {
+  class DropdownDate {
     constructor(component) {
       this.component = component;
-      this.$start = $('.js-date-dropdown__start', this.$component);
-      this.$end = $('.js-date-dropdown__end', this.$component);
+      this.$start = $('.js-dropdown-date__start', this.$component);
+      this.$end = $('.js-dropdown-date__end', this.$component);
       this._attachEventHandler(this.$start,this.$end);
     }
     _attachEventHandler(start,end){
         $(start).datepicker({
           todayButton: new Date(),
-          //-range: $(this).component,
           onSelect: function (formattedDate, date, inst) {
-            const [ firstDate, secondDate ] = fd.split(inst.views.days.opts.multipleDatesSeparator);
+            let separator= inst.views.days.opts.multipleDatesSeparator;
+            const [ firstDate, secondDate ] = formattedDate.split(separator);
             $(start).val(firstDate);
             $(end).val(secondDate);
           },
     }).data('datepicker');
     }
   }
-    $('.js-date-dropdown').each((index, node) => {
-      new DateDropdown(node);
+    $('.js-dropdown-date').each((index, elem) => {
+      new DropdownDate(elem);
   });
 });
 
