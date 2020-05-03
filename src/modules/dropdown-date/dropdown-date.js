e0d99762fbd7806614ec0de50661ea88fbf9c847
Amferiel <incanu94@mail.com>
2020-05-03 14:59:04 +0300

    Полностью дописан dropdown-date

--- src/modules/dropdown-date/dropdown-date.js
+++ src/modules/dropdown-date/dropdown-date.js
@@ -9,53 +9,52 @@ $(() => {
       this.createDatepicker(this.component);
       this.attachEventHundlers();
     }
+
     createDatepicker(component) {
       this.$fields = $('.dropdown-date__input', this.component);
       this.$start = $('.js-dropdown-date__start', this.component);
       this.$end = $('.js-dropdown-date__end', this.component);
-      console.log(this.$fields);
-      //options for dropdown-date with one input
+      //options for datepicker
       const options = {
         prevHtml: '<i class="material-icons">arrow_back</i>',
-        nextHtml: '<i class="material-icons">arrow_forward</i>'
-      }
-      //options for dropdown-date with two inputs
-      const optionsTwoFields = {
-        prevHtml: '<i class="material-icons">arrow_back</i>',
         nextHtml: '<i class="material-icons">arrow_forward</i>',
-        onSelect: function (formattedDate, date, inst) {
-          //-let separator = inst.views.days.opts.multipleDatesSeparator;
-          let separator = " - ";
-          const [firstDate, secondDate] = formattedDate.split(separator);
-          $(this.$fields[0]).val(firstDate);
-          
-          $(this.$fields[1]).val(secondDate);
-        }
+        onSelect: this.onSelect.bind(this)
       }
-      if($(component).hasClass('dropdown-date_twoFields')){
-        this.datepicker= $(this.$start).datepicker(optionsTwoFields).data('datepicker')
-      }else{
-        this.datepicker= $(this.$fields).datepicker(options).data('datepicker');
+      this.datepicker = $(this.$fields[0]).datepicker(options).data('datepicker');
+
+      //-if datepicker is static showed then hide input with label
+      if ($(this.$fields).attr('data-inline') === 'true') {
+        $(this.$fields).closest('.js-dropdown-date').css('display', 'none');
       }
-      console.log($(component).hasClass('dropdown-date_twoFields'));
       //add buttons
       let buttonReset = "<div class='button-reset'><button class='button button_link'>Очистить</button></div>";
       let buttonApply = "<div class='button-apply'><button class='button button_link'>Применить</button></div>";
       let controls = $('<div class="dropdown-date__controls">' + buttonReset + buttonApply + '</div>');
 
-     $(this.datepicker.$datepicker).append($(controls));
+      $(this.datepicker.$datepicker).append($(controls));
+
+      this.$buttonReset = $(controls).find('.button-reset');
+      this.$buttonApply = $(controls).find('.button-apply');
+    }
+    attachEventHundlers() {
+      this.$buttonApply.on('click', this.toApply.bind(this));
+      this.$buttonReset.on('click', this.toReset.bind(this));
+    }
+
+    toApply() {
+      this.datepicker.hide();
+    }
+
+    toReset() {
+      this.datepicker.clear();
+    }
+
+    onSelect(formattedDate, date, inst) {
+      let separator = inst.views.days.opts.multipleDatesSeparator;
+      const [firstDate, secondDate] = formattedDate.split(separator);
       
-    }
-    attachEventHundlers(){
-      $('.dropdown-date__controls .button-apply', this.component).on('click', this.toApply.bind(this));
-      debugger;
-      $('.dropdown-date__controls .button-reset', this.component).on('click', this.toReset.bind(this));
-    }
-    toApply(e){
-      $(this.datepicker.$datepicker).hide();
-    }
-    toReset(){
-      $(this.datepicker.$datepicker).clear();
+      $(this.$start).val(firstDate);
+      $(this.$end).val(secondDate);
     }
   }
 
