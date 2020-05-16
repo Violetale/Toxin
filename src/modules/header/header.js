e04684ce75c3992ef9b9dfe30d233a00d4b71555
Amferiel <incanu94@mail.com>
2020-05-16 22:22:08 +0300

    Рефакторинг header. Добавлен респонсив и поведение для кнопок мобильного меню

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -1,1 +1,9 @@
 import './header.scss';
+$(()=>{
+    $('.js-user-button').on('click',showUserButtons);
+    function showUserButtons(e){
+        $(this).siblings('.header__buttons').toggleClass('header__buttons_open');
+    }
+}
+
+);
